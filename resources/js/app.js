4ef24cb22f29a541e0682855738a6e301f17f69d
Asper <asper@strategicsale.com>
2019-12-20 09:25:57 +0800

    mediamanager vendor install

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,6 +28,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+require('../assets/vendor/MediaManager/js/manager')
+
 const app = new Vue({
     el: '#app'
 });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// MediaManager
+ctf0\MediaManager\MediaRoutes::routes();
