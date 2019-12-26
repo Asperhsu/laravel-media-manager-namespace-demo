6d7ee0b1ccea61a2788624243dcc861ce6bad7b5
Asper <asper@strategicsale.com>
2019-12-26 10:16:11 +0800

    edit scss for bootstrap demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous" />
 </head>
 <body>
     <div id="app" style="padding: 2rem; height: 100vh; display: flex; align-items: center; justify-content: center;">
